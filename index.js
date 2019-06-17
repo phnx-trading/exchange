fb7e6e5fb30ca469bc2ef74fbcffa4dc18e3ffc1
Joah Gerstenberg <me@joahg.com>
2019-06-16 19:56:01 -0500

    adapt for lambda

--- index.js
+++ index.js
@@ -3,11 +3,15 @@
 const express = require(`express`),
       v1 = require(`./v1`);
 
-const app = express(),
-      PORT = process.env.PORT || 3000;
+const app = express();
 
 app.use(`/v1`, v1);
 
-app.listen(PORT, () => {
-  console.log(`http server listening on port ${ PORT }`)
-});
+if (!!process.env.LAMBDA_TASK_ROOT) {
+  const serverlessExpress = require('aws-serverless-express'),
+        server = serverlessExpress.createServer(app);
+
+  exports.main = (event, context) => serverlessExpress.proxy(server, event, context);
+} else {
+  app.listen(3000, () => console.log(`Listening on 3000`));
+}
